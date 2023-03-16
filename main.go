6cc0f2846cd60b130aff4514cf95b8930c8eb71d
Mikolaj Umanski <mumanski@microsoft.com>
2023-03-16 13:37:47 -0700

    test

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func subtractNumbers(a, b int) int {
 	return a - b
 }
 
+func multiplyNumbers(a, b int) int {
+	return a * b
+}
+
 func main() {
 	s := addNumbers(1, 2)
 	d := subtractNumbers(1, 2)
-	fmt.Printf("sum is %d, difference is %d", s, d)
+	m := multiplyNumbers(1, 2)
+	fmt.Printf("sum is %d, difference is %d, multiplied %d", s, d, m)
+	fmt.Println("test PR")
 }
