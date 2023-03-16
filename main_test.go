99b0acb0b10e3abd3a6724b88a75c07322aa09d1
Mikolaj Umanski <mumanski@microsoft.com>
2023-03-16 15:21:15 -0700

    some more tests...

--- main_test.go
+++ main_test.go
@@ -8,3 +8,17 @@ func TestAddNumbers(t *testing.T) {
 		t.Errorf("addNumbers(1, 2) = %d; want 3", result)
 	}
 }
+
+func TestSubtractNumbers(t *testing.T) {
+	result := subtractNumbers(1, 2)
+	if result != -1 {
+		t.Errorf("subtractNumbers(1, 2) = %d; want -1", result)
+	}
+}
+
+func TestMultiplyNumbers(t *testing.T) {
+	result := multiplyNumbers(1, 2)
+	if result != 2 {
+		t.Errorf("multiplyNumbers(1, 2) = %d; want 2", result)
+	}
+}
